d8f42d24a9bae3f2051e067be6f5789984f64e9f
diegoPaladino <diegopaladinofoto@gmail.com>
2020-09-03 16:03:36 -0300

    202009031603

---
+++ foramarinho_auto_bot.py
@@ -0,0 +1,21 @@
+#foramarinho_auto_bot
+
+import pyautogui as p
+import time as t
+import schedule as s
+import winsound as w
+
+p.moveTo(1085,-249)  #<<<<<<<<<<<<<<<<<<<<<<<<<<< point of chat
+p.click()
+t.sleep(0.3)
+
+def job():
+    p.write('#FORAMARINHO')
+    p.hotkey('enter')
+    w.Beep(frequency=2500,duration=1000)
+
+s.every().minute.do(job)
+
+while True:
+    s.run_pending()
+    t.sleep(1)
