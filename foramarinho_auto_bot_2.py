3d40fd6154ac3df58f5c3064849a9f781d50056a
diegoPaladino <diegopaladinofoto@gmail.com>
2021-06-14 07:43:49 -0300

    202106140743

--- foramarinho_auto_bot_2.py
+++ foramarinho_auto_bot_2.py
@@ -10,14 +10,14 @@ p.moveTo(x=1216, y=-240)  #<<<<<<<<<<<<<<<<<<<<<<<<<<< point of chat
 p.click()
 t.sleep(0.3)
 
-p.write('#FORAMARINHO - ISRAEL TEAM')
+p.write('MANAUARA - Fefito do Amazonas/Norte. HUUUUM VAAAI CARNEIRIIIIIIIIIIIINHO!!! #ForaMarinho')
 t.sleep(4)
 p.hotkey('enter')
 w.Beep(frequency=2500,duration=1000)
 print (datetime.now())
 
 def job():
-    p.write('#FORAMARINHO - ISRAEL TEAM')
+    p.write('MANAUARA - Fefito do Amazonas/Norte. HUUUUM VAAAI CARNEIRIIIIIIIIIIIINHO!!! #ForaMarinho')MANAUARA - Fefito do Amazonas/Norte. HUUUUM VAAAI CARNEIRIIIIIIIIIIIINHO!!! #ForaMarinho
             
             
     t.sleep(4)
